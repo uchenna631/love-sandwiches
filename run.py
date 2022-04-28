fd2edf37619aff6f34440c2d834f19016ce721dc
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-04-28 15:06:07 +0000

    Create get_sales_data function to collect sales data

--- run.py
+++ run.py
@@ -14,4 +14,17 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
-print(data)
+
+def get_sales_data():
+    """
+    Get sales data from the user
+    """
+
+    print('Please enter sales data from the last market')
+    print('The sales data must be six numbers, separated by a comma')
+    print('Example: 10, 20, 30, 40, 50, 60\n')
+
+    data_str = input('Enter your data here: ')
+    print(f'The data provided is: {data_str}')
+
+get_sales_data()
