3f1f8f84bc920829fb73568e4b26372e7336d302
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-04-28 15:39:49 +0000

    Create validate_data function to check if the length of data provided is 6

--- run.py
+++ run.py
@@ -25,6 +25,25 @@ def get_sales_data():
     print('Example: 10, 20, 30, 40, 50, 60\n')
 
     data_str = input('Enter your data here: ')
-    print(f'The data provided is: {data_str}')
+
+    sales_data = data_str.split(',')
+    print(sales_data)
+    validate_data(sales_data)
+
+def validate_data(value):
+    """
+    Inside try, convert all string values to integers.
+    Raise ValueError if strings cannot be converted to int, 
+    or len of string not equal to 6
+    """
+
+    try:
+        if len(value) != 6:
+            raise ValueError(
+                f'Exactly 6 values required, you provided only {len(value)}'
+            )
+    except ValueError as e:
+        print(f'Invalid data: {e}, Please try again.\n')
 
 get_sales_data()
+
