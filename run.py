7cd6f03ffa7aefe0b25a16d667ae7d12876b2086
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-04-29 12:06:26 +0000

    Retrieve the latest stock number from the worksheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -62,6 +63,24 @@ def update_sales_worksheet(data):
     
     print('Sales worksheet updated successfully\n')
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_data):
+    """
+    Function to calculate surplus
+    """
+    print('Calculating surplus data\n')
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    pprint(stock_row)
+
+
+def main():
+    """
+    Run all our program
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print('Welcome to Love Sandwiches Data Automation')
+main()
