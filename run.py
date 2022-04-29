fad77cdd1225271960bb92f84a2195a958383a74
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-04-29 11:17:35 +0000

    Create function to update sales worksheet

--- run.py
+++ run.py
@@ -51,5 +51,17 @@ def validate_data(values):
         print(f'Invalid data: {e}, Please try again.\n')
         return False
     return True
+
+def update_sales_worksheet(data):
+    """
+    Function to update the sales worksheet
+    """
+    print('Updating sales worksheet...\n')
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
     
+    print('Sales worksheet updated successfully\n')
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
